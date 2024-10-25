57c7aa48dd62755940e7c714fc86a115bb25a2c3
Theresa <ymy.theresa@gmail.com>
2024-10-26 04:05:43 +0800

    async http request

--- taskReqGenerator.sh
+++ taskReqGenerator.sh
@@ -1,64 +1,32 @@
-# #!/bin/bash
-
-# url="http://localhost:8080/task"
-
-# for i in {1..100}
-# do
-#   request_id="$i"
-
-#   # Add timestamp
-#   timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-#   echo "Sending request: {\"request_id\": \"$request_id\"} at $timestamp"
-
-#   curl -X POST "$url" \
-#     -H "Content-Type: application/json" \
-#     -d "{\"request_id\": \"$request_id\"}"
-
-# done
-
-# echo "100 tasks submitted."
 #!/bin/bash
 
-# URL to send POST requests to
 url="http://localhost:8080/task"
 
-# Total number of requests to send
 total_requests=100
 
-# Maximum number of concurrent requests
-max_concurrent=10
+output_file="responses.txt"
 
-# Function to send a single POST request
 send_request() {
   local request_id=$1
   local url=$2
 
-  # Add timestamp
   timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-  echo "Sending request: {\"request_id\": \"$request_id\"} at $timestamp"
 
-  # Send POST request
-  curl -X POST "$url" \
+  request_payload="{\"request_id\": \"$request_id\"}"
+
+  echo "[$timestamp] Sending request: $request_payload" | tee -a "$output_file"
+
+  response=$(curl -s -X POST "$url" \
     -H "Content-Type: application/json" \
-    -d "{\"request_id\": \"$request_id\"}" &
+    -d "$request_payload")
+
+  echo "[$timestamp] Request ID $request_id: $response" | tee -a "$output_file"
 }
 
-# Initialize a counter for concurrent jobs
-current_jobs=0
-
-# Loop to send requests
 for i in $(seq 1 "$total_requests"); do
-  send_request "$i" "$url"
-  current_jobs=$((current_jobs + 1))
-
-  # If the maximum number of concurrent jobs is reached, wait for any to finish
-  if [ "$current_jobs" -ge "$max_concurrent" ]; then
-    wait -n
-    current_jobs=$((current_jobs - 1))
-  fi
+  send_request "$i" "$url" &
 done
 
-# Wait for all remaining background jobs to finish
 wait
 
-echo "$total_requests tasks submitted."
+echo "$(date +"%Y-%m-%d %H:%M:%S") $total_requests tasks submitted." | tee -a "$output_file"
